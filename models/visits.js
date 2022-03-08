4db9cf44a42cacf99a7f937cd6f901cf9a0f0943
Robert Ward <raward4@ncsu.edu>
2022-03-08 09:46:00 -0500

    corrected visits typos in models and routes

--- models/visits.js
+++ models/visits.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 const Schema = mongoose.Schema
-const tacoSchema = new Schema({
+const visitSchema = new Schema({
   name: String,
   tasty: Boolean,
   owner: {type: Schema.Types.ObjectId, ref: "Profile"}
--- routes/visits.js
+++ routes/visits.js
@@ -1,27 +1,27 @@
 import { Router } from 'express'
-import * as tacosCtrl from "../controllers/visits.js"
+import * as visitsCtrl from "../controllers/visits.js"
 import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 // GET - localhost:3000/tacos
-router.get('/', tacosCtrl.index)
+router.get('/', visitsCtrl.index)
 // GET - localhost:3000/tacos/:id
-router.get("/:id", tacosCtrl.show)
+router.get("/:id", visitsCtrl.show)
 // GET - localhost:3000/tacos/:id/edit
-router.get("/:id/edit", isLoggedIn, tacosCtrl.edit)
+router.get("/:id/edit", isLoggedIn, visitsCtrl.edit)
 
 // POST - localhost:3000/tacos
-router.post('/', isLoggedIn, tacosCtrl.create)
+router.post('/', isLoggedIn, visitsCtrl.create)
 
 // PUT - localhost:300/taco/:id
-router.put("/:id", isLoggedIn, tacosCtrl.update)
+router.put("/:id", isLoggedIn, visitsCtrl.update)
 
 // PATCH - localhost:3000/tacos/:id/flip-tasty
-router.patch("/:id/flip-tasty", isLoggedIn, tacosCtrl.flipTasty)
+router.patch("/:id/flip-tasty", isLoggedIn, visitsCtrl.flipTasty)
 
 // DELETE - localhost:3000/tacos/:id
-router.delete("/:id", isLoggedIn, tacosCtrl.delete)
+router.delete("/:id", isLoggedIn, visitsCtrl.delete)
 
 export {
   router
